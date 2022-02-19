c13797cd0e07bf0a205d3f8d58038c2fd97262fd
Shivam Verma <shivamverma01123@gmail.com>
2022-02-19 13:30:58 -0500

    cracked algorithm

--- bots/template_bot.py
+++ bots/template_bot.py
@@ -16,6 +16,30 @@ class MyPlayer(Player):
 
 
     def play_turn(self, turn_num, map, player_info):
+        self.MAP_WIDTH = len(map)
+        self.MAP_HEIGHT = len(map[0])
+        k = 3
+        # consider 1 generator 
+        # consider where the generator is 
+        # do generators have ids? 
+        # can you build cell tower on road?
 
-
+        # step 1: construct a list of nodes 
+        # 0, 0 -> 60, 60 
+        # pad grid up to multiples of 5 
+        nodes = []
+        for a in range(self.MAP_WIDTH // k): 
+            for b in range(self.MAP_HEIGHT // k): 
+                x = a * k
+                y = b * k 
+                total_terrain = 0
+                total_population = 0
+                for i in range(k): 
+                    for j in range(k): 
+                        total_terrain += map[x + i][y + j].passability 
+                        total_population += map[x + i][y + j].population
+                nodes.append((x, y, total_terrain, total_population))
+        print(nodes)
+        if(turn_num > 1): 
+            assert(1 == 2)
         return
